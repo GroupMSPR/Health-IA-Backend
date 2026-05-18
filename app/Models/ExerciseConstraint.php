1ab24d5caa58469fe5a7e9d47f1c0fc67e2317c2
Diana09999 <153197938+Diana09999@users.noreply.github.com>
2026-05-18 20:55:12 +0200

    update

--- app/Models/ExerciseConstraint.php
+++ app/Models/ExerciseConstraint.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class ExerciseConstraint extends Model
+class ExerciseConstraint extends Pivot
 {
-    //
+    protected $table = 'exercise_constraints';
+
+    protected $primaryKey = 'id';
+
+    public function exercise(): BelongsTo
+    {
+        return $this->belongsTo(Exercise::class);
+    }
+
+    public function constraint(): BelongsTo
+    {
+        return $this->belongsTo(Constraint::class);
+    }
 }
