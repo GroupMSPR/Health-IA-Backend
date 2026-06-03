7e67bc2be3ca58911a51b1d75f4f9c483c52e997
Diana09999 <153197938+Diana09999@users.noreply.github.com>
2026-06-03 08:34:36 +0200

    add entites policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,13 +2,21 @@
 
 namespace App\Providers;
 
+use App\Models\Equipment;
 use App\Models\Exercise;
 use App\Models\Food;
+use App\Models\Goal;
 use App\Models\HealthMetric;
+use App\Models\Muscle;
+use App\Models\Subscription;
 use App\Models\User;
+use App\Policies\EquipmentPolicy;
 use App\Policies\ExercisePolicy;
 use App\Policies\FoodPolicy;
+use App\Policies\GoalPolicy;
 use App\Policies\HealthMetricPolicy;
+use App\Policies\MusclePolicy;
+use App\Policies\SubscriptionPolicy;
 use App\Policies\UserPolicy;
 
 class AuthServiceProvider extends \Illuminate\Foundation\Support\Providers\AuthServiceProvider
@@ -18,6 +26,10 @@ class AuthServiceProvider extends \Illuminate\Foundation\Support\Providers\AuthS
         Food::class => FoodPolicy::class,
         Exercise::class => ExercisePolicy::class,
         HealthMetric::class => HealthMetricPolicy::class,
+        Goal::class => GoalPolicy::class,
+        Muscle::class => MusclePolicy::class,
+        Subscription::class => SubscriptionPolicy::class,
+        Equipment::class => EquipmentPolicy::class,
     ];
 
     /**
