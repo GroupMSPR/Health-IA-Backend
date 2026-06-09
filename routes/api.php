ff35b9cbce0be2e2fafeb9cee5dd35149918f6d3
Ilan ARFI <i.arfipro@outlook.fr>
2026-06-09 22:34:11 +0200

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -32,8 +32,8 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
         return response()->json($request->user());
     });
 
-    Route::get('/exercises/count', function () {return response()->json(DB::table('exercises')->count());});
-    Route::get('/foods/count', function () { return response()->json(DB::table('foods')->count());});
+    Route::get('exercises/count', function () { return response()->json(DB::table('exercises')->count()); });
+    Route::get('foods/count', function () { return response()->json(DB::table('foods')->count()); });
 
     Rest::resource('users', UsersController::class)->withSoftDeletes();
     Rest::resource('foods', FoodsController::class)->withSoftDeletes();
