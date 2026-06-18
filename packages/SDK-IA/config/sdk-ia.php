57226afa2232e6601ed6378aa2b4cac2a1cd1e2f
Diana09999 <dianakolo888@gmail.com>
2026-06-18 13:33:00 +0200

    add timeout 240s

--- packages/SDK-IA/config/sdk-ia.php
+++ packages/SDK-IA/config/sdk-ia.php
@@ -3,7 +3,7 @@
 return [
     'fastapi' => [
         'url' => env('IA_URL', 'http://healthai_fastapi:4000'),
-        'timeout-ollama' => env('OLLAMA_TIMEOUT', 120),
+        'timeout-ollama' => env('OLLAMA_TIMEOUT', 240),
         'timeout-ml' => env('ML_SERVICE_TIMEOUT', 120),
     ],
 ];
