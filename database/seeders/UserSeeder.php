310a6f6aacad4c8be07a11d38f500fd72300431c
Ilan ARFI <i.arfipro@outlook.fr>
2026-06-19 03:20:46 +0200

    fix

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,7 +12,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        // User::factory(10)->create();
 
         User::factory()->create([
             'id' => '019edc60-6a3f-73ec-afde-13d362eea794',
