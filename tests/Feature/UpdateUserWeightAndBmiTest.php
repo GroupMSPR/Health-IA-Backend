b309bdec36016a41da1f9a995a4bbd6b8816f1a6
Diana09999 <dianakolo888@gmail.com>
2026-06-26 20:57:57 +0200

    make TU UpdateUserWeightAndBmiTest.php

--- tests/Feature/UpdateUserWeightAndBmiTest.php
+++ tests/Feature/UpdateUserWeightAndBmiTest.php
@@ -11,8 +11,12 @@ use Tests\TestCase;
 class UpdateUserWeightAndBmiTest extends TestCase
 {
     use RefreshDatabase;
+
     /**
-     * A basic unit test example.
+     *  Poids et BMI mis à jour correctement
+     *
+     * User : 63kg, 170cm
+     * BMI attendu = 63 / (1.70²) = 63 / 2.89 = 21.80
      */
     public function test_weight_and_bmi_are_updated_when_health_metric_created(): void
     {
@@ -36,5 +40,69 @@ class UpdateUserWeightAndBmiTest extends TestCase
 
         $expectedBmi = round(63 / (1.70 ** 2), 2);
         $this->assertEquals($expectedBmi, $user->bmi);
+    }
+
+    /******
+     *
+     * Si weight est null -> rien se passe
+     *****/
+    public function test_nothing_happens_when_weight_is_null(): void
+    {
+        $user = User::factory()->create([
+            'weight' => 50,
+            'bmi'    => 20,
+        ]);
+
+        $this->actingAs($user, 'sanctum');
+
+        $metric = new HealthMetric();
+        $metric->weight = null;
+
+        $listener = new UpdateUserWeightAndBmi();
+        $listener->handle($metric);
+
+        $user->refresh();
+
+        $this->assertEquals(50, $user->weight);
+        $this->assertEquals(20, $user->bmi);
+    }
+
+    /******
+     *
+     * Si height est 0 -> BMI non calculé mais poids mis a jour
+     *****/
+
+    public function test_weight_updated_but_bmi_not_calculated_when_height_is_zero(): void
+    {
+        $user = User::factory()->create([
+            'weight' => 50,
+            'height' => 0,
+            'bmi' => 0,
+        ]);
+
+        $this->actingAs($user, 'sanctum');
+
+        $metric = new HealthMetric();
+        $metric->weight = 70;
+
+        $listener = new UpdateUserWeightAndBmi();
+        $listener->handle($metric);
+
+        $user->refresh();
+
+        $this->assertEquals(70, $user->weight);
+        $this->assertEquals(0, $user->bmi);
+    }
+
+    public function test_nothing_happens_when_no_authenticated_user(): void
+    {
+        $metric = new HealthMetric();
+        $metric->weight = 70;
+
+        $listener = new UpdateUserWeightAndBmi();
+
+        $listener->handle($metric);
+
+        $this->assertTrue(true);
     }
 }
