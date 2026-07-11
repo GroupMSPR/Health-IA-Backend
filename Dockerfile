6bb4aabf815f953b6d9257abdcd3665bf6802b87
Ilan ARFI <i.arfipro@outlook.fr>
2026-07-11 17:16:09 +0200

    add redis/horizon for jobs & cache

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ FROM dunglas/frankenphp:1-php8.4-alpine
 RUN install-php-extensions \
         pdo_pgsql \
         pgsql \
+        redis \
         zip \
         pcntl \
         bcmath \
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,11 +3,13 @@
 use App\Providers\AppServiceProvider;
 use App\Providers\AuthServiceProvider;
 use App\Providers\Filament\AdminPanelProvider;
+use App\Providers\HorizonServiceProvider;
 use L5Swagger\L5SwaggerServiceProvider;
 
 return [
     AppServiceProvider::class,
     AuthServiceProvider::class,
     AdminPanelProvider::class,
+    HorizonServiceProvider::class,
     L5SwaggerServiceProvider::class,
 ];
